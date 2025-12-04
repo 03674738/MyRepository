41db07a290cb1b5258a6e83626a70ac56109372e
admin <admin@DESKTOP-I6HUGBR>
2025-12-04 14:27:18 +0900

    change in master branch

--- GitHW1/src/mygit/MyClass.java
+++ GitHW1/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("2nd");
+		System.out.println("MyClass is edited in master branch");
 	}
 }
